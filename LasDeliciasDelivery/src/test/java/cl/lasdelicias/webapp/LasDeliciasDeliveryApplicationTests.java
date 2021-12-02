7dc062c826f19596e0c6a00ed0b4fe46d56593ce
root <root@adareloise-ThinkPad-X220>
2021-12-01 21:02:18 -0300

    modify test

--- LasDeliciasDelivery/src/test/java/cl/lasdelicias/webapp/LasDeliciasDeliveryApplicationTests.java
+++ LasDeliciasDelivery/src/test/java/cl/lasdelicias/webapp/LasDeliciasDeliveryApplicationTests.java
@@ -1,10 +1,10 @@
-package cl.barrita.webapp;
+package cl.lasdelicias.webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BarritaDeliveryApplicationTests {
+class LasDeliciasDeliveryApplicationTests {
 
 	@Test
 	void contextLoads() {
